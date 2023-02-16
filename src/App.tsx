49d3aea24c52a7993249bfe1cf1acc32dcac94e2
brettdixon <bdix26@gmail.com>
2023-02-16 14:45:48 -0700

    initial add

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Navbar} from "./layouts/navbarAndFooter/Navbar";
 
-function App() {
+import {Footer} from "./layouts/navbarAndFooter/Footer";
+import {HomePage} from "./layouts/HomePage/HomePage";
+
+export const App = () =>{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <Navbar />
+            <HomePage/>
+          <Footer />
+      </div>
   );
 }
 
-export default App;
